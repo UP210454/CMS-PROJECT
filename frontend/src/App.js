85c6812e9836cde8c2a9557fc037d9da22502dfe
Luis Fernando de la Cruz Robledo <up210454@alumnos.upa.edu.mx>
2024-07-16 19:54:59 -0600

    Añadir noticia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main'
 import Login from './views/login'
+import AñadirNota from './views/añadirNota';
 
 //Views
 
@@ -16,6 +17,7 @@ function App() {
 					<Routes>
 						<Route path='/login' element={<Login/>}></Route>
 						<Route path='/home' element={<Main/>}></Route>
+						<Route path='/addNotice' element={<AñadirNota/>}></Route>
 					</Routes>
 				</Fragment>
 			</BrowserRouter>
--- frontend/src/views/añadirNota.js
+++ frontend/src/views/añadirNota.js
@@ -2,106 +2,110 @@ import React from "react";
 import { Container, TextField, Box } from '@mui/material';
 function AñadirNota () {
     return (
-        <Container
-        maxWidth="sm"
-        sx={{
-          marginTop: '20px',
-          backgroundColor: 'black',
-          padding: '20px',
-          borderRadius: '8px',
-        }}
-      >
-        <Box component="form" noValidate autoComplete="off">
-          <TextField
-            fullWidth
-            label="Título"
-            variant="outlined"
-            margin="normal"
-            InputLabelProps={{ style: { color: 'white' } }}
-            InputProps={{ style: { color: 'white' } }}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': { borderColor: 'white' },
-                '&:hover fieldset': { borderColor: 'white' },
-                '&.Mui-focused fieldset': { borderColor: 'white' }
-              },
-              '& .MuiInputBase-input': { color: 'white' },
-              '& .MuiFormLabel-root': { color: 'white' }
-            }}
-          />
-          <TextField
-            fullWidth
-            label="Fecha"
-            type="date"
-            variant="outlined"
-            margin="normal"
-            InputLabelProps={{ shrink: true, style: { color: 'white' } }}
-            InputProps={{ style: { color: 'white' } }}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': { borderColor: 'white' },
-                '&:hover fieldset': { borderColor: 'white' },
-                '&.Mui-focused fieldset': { borderColor: 'white' }
-              },
-              '& .MuiInputBase-input': { color: 'white' },
-              '& .MuiFormLabel-root': { color: 'white' }
-            }}
-          />
-          <TextField
-            fullWidth
-            label="Autor"
-            variant="outlined"
-            margin="normal"
-            InputLabelProps={{ style: { color: 'white' } }}
-            InputProps={{ style: { color: 'white' } }}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': { borderColor: 'white' },
-                '&:hover fieldset': { borderColor: 'white' },
-                '&.Mui-focused fieldset': { borderColor: 'white' }
-              },
-              '& .MuiInputBase-input': { color: 'white' },
-              '& .MuiFormLabel-root': { color: 'white' }
-            }}
-          />
-          <TextField
-            fullWidth
-            label="URL de imagen"
-            variant="outlined"
-            margin="normal"
-            InputLabelProps={{ style: { color: 'white' } }}
-            InputProps={{ style: { color: 'white' } }}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': { borderColor: 'white' },
-                '&:hover fieldset': { borderColor: 'white' },
-                '&.Mui-focused fieldset': { borderColor: 'white' }
-              },
-              '& .MuiInputBase-input': { color: 'white' },
-              '& .MuiFormLabel-root': { color: 'white' }
-            }}
-          />
-          <TextField
-            fullWidth
-            label="Contenido"
-            variant="outlined"
-            margin="normal"
-            multiline
-            rows={10}
-            InputLabelProps={{ style: { color: 'white' } }}
-            InputProps={{ style: { color: 'white' } }}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': { borderColor: 'white' },
-                '&:hover fieldset': { borderColor: 'white' },
-                '&.Mui-focused fieldset': { borderColor: 'white' }
-              },
-              '& .MuiInputBase-input': { color: 'white' },
-              '& .MuiFormLabel-root': { color: 'white' }
-            }}
-          />
-        </Box>
-      </Container>
+        <div>
+          <br/>
+          <br/>
+          <Container
+          maxWidth="sm"
+          sx={{
+            marginTop: '20px',
+            backgroundColor: 'black',
+            padding: '20px',
+            borderRadius: '8px',
+          }}
+        >
+          <Box component="form" noValidate autoComplete="off">
+            <TextField
+              fullWidth
+              label="Título"
+              variant="outlined"
+              margin="normal"
+              InputLabelProps={{ style: { color: 'white' } }}
+              InputProps={{ style: { color: 'white' } }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: 'white' },
+                  '&:hover fieldset': { borderColor: 'white' },
+                  '&.Mui-focused fieldset': { borderColor: 'white' }
+                },
+                '& .MuiInputBase-input': { color: 'white' },
+                '& .MuiFormLabel-root': { color: 'white' }
+              }}
+            />
+            <TextField
+              fullWidth
+              label="Fecha"
+              type="date"
+              variant="outlined"
+              margin="normal"
+              InputLabelProps={{ shrink: true, style: { color: 'white' } }}
+              InputProps={{ style: { color: 'white' } }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: 'white' },
+                  '&:hover fieldset': { borderColor: 'white' },
+                  '&.Mui-focused fieldset': { borderColor: 'white' }
+                },
+                '& .MuiInputBase-input': { color: 'white' },
+                '& .MuiFormLabel-root': { color: 'white' }
+              }}
+            />
+            <TextField
+              fullWidth
+              label="Autor"
+              variant="outlined"
+              margin="normal"
+              InputLabelProps={{ style: { color: 'white' } }}
+              InputProps={{ style: { color: 'white' } }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: 'white' },
+                  '&:hover fieldset': { borderColor: 'white' },
+                  '&.Mui-focused fieldset': { borderColor: 'white' }
+                },
+                '& .MuiInputBase-input': { color: 'white' },
+                '& .MuiFormLabel-root': { color: 'white' }
+              }}
+            />
+            <TextField
+              fullWidth
+              label="URL de imagen"
+              variant="outlined"
+              margin="normal"
+              InputLabelProps={{ style: { color: 'white' } }}
+              InputProps={{ style: { color: 'white' } }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: 'white' },
+                  '&:hover fieldset': { borderColor: 'white' },
+                  '&.Mui-focused fieldset': { borderColor: 'white' }
+                },
+                '& .MuiInputBase-input': { color: 'white' },
+                '& .MuiFormLabel-root': { color: 'white' }
+              }}
+            />
+            <TextField
+              fullWidth
+              label="Contenido"
+              variant="outlined"
+              margin="normal"
+              multiline
+              rows={10}
+              InputLabelProps={{ style: { color: 'white' } }}
+              InputProps={{ style: { color: 'white' } }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: 'white' },
+                  '&:hover fieldset': { borderColor: 'white' },
+                  '&.Mui-focused fieldset': { borderColor: 'white' }
+                },
+                '& .MuiInputBase-input': { color: 'white' },
+                '& .MuiFormLabel-root': { color: 'white' }
+              }}
+            />
+          </Box>
+        </Container>
+        </div>
     );
 }
 export default AñadirNota;
