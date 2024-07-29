728c80646e454ed9dff2419212f6b9eb03dfa694
UP210647 <up210647@alumnos.upa.edu.mx>
2024-07-28 18:07:11 -0600

    Altas funcionando

--- frontend/src/components/NoticiasAdmin/NoticiasAdmin.js
+++ frontend/src/components/NoticiasAdmin/NoticiasAdmin.js
@@ -4,14 +4,14 @@ const NoticiasAdmin = ({titulo, resumen, id}) => {
   return (
     <div className="noticiaComponent">  
         <div className="detallesNoticia">
-          <h1>{titulo}</h1>
+          <h3>{titulo}</h3>
           <p>{resumen}</p>
         </div>
         <div className="botones">
-          <button className="buttonEdit">Update</button>      
+          <button className="buttonEdit"><img src="lapiz.png" class="updateImg"></img></button>      
         </div>
         <div className="botones">
-          <button className="buttonEdit">Dlt</button>
+          <button className="buttonEdit"><img src="borrar.png" class="deleteImg"></img></button>
         </div>
     </div>
   );
